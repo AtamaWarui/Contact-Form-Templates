435e9c7da3645f8dbc67eb31bebdd33ea73ce4bf
Mounia Janah <89919019+MoonJanah@users.noreply.github.com>
2021-12-10 23:02:23 +0900

    Add files via upload

---
+++ ContactForm-2/ContactForm1.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML>
+                                                                                                      
+<html>
+
+<!-- Head-->
+
+	<head>
+		<title>Contact Form Template</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/mainCF1.css" />
+	</head>
+
+
+<!-- Java Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+<!-- Body -->
+
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+<!-- Highlights 1-->
+	
+        <section id="highlights" class="wrapper style3">				
+	  
+
+	</section>
+
+<!-- Contact Form -->
+				
+
+        <section id="footer" class="wrapper">
+	   <div id="Lorem_Ipsum1" class="title">Contact Us</div>
+		 <div class="container">
+		     <header class="style1">
+			 <h2>Please ask any questions?</h2>
+			     <p>
+				For more informations, please contact us.<br />
+			     </p>
+				</header>
+				  <div class="row">
+				    <div class="col-6 col-12-medium">
+<!-- Contact Form 2 -->
+	
+        <section>
+	   <form method="post" action="#">
+		<div class="row gtr-50">
+		   <div class="col-6 col-12-small">
+			<input type="text" name="name" id="contact-name" placeholder="Name" />
+		   </div>
+		   <div class="col-6 col-12-small">
+			<input type="text" name="email" id="contact-email" placeholder="Email" />
+		   </div>
+		   <div class="col-12">
+			<textarea name="message" id="contact-message" placeholder="Message" rows="4"></textarea>
+		   </div>
+		   <div class="col-12">
+			<ul class="actions">
+			<li><input type="submit" class="style1" value="Send" /></li>
+			<li><input type="reset" class="style2" value="Reset" /></li>
+			</ul>
+		   </div>
+		 </div>
+	   </form>
+	</section>
+
+		  </div>
+               <div class="col-6 col-12-medium">
+
+<!-- Contact -->
+
+	<section class="feature-list small">
+	       <div class="row">
+		  <div class="col-6 col-12-small">
+	<section>
+	       <h3 class="icon solid fa-home">Address</h3>
+		  <p>
+		     Kawasaki, kanagawa-ken<br />
+                     Japan<br />
+		  </p>
+	</section>
+		 </div>
+	      <div class="col-6 col-12-small">
+	<section>
+	       <h3 class="icon solid fa-comment">Social Media</h3>
+		  <p>
+		    <a href="https://jp.linkedin.com/in/mouniajanah">linkedin.com/mouniajanah</a><br />
+		  </p>
+	</section>
+	      </div>
+	     <div class="col-6 col-12-small">
+	<section>
+	       <h3 class="icon solid fa-envelope">Email</h3>
+		  <p>
+		    <a href="#">example@gmail.com</a>
+		  </p>      
+	</section>
+	      </div>
+	     <div class="col-6 col-12-small">
+	<section>
+	       <h3 class="icon solid fa-phone">Contact Number</h3>
+		  <p>
+		     (080) 05262-5875
+		  </p>
+	</section>
+	     </div>
+		 </div>
+
+        </section>
+                 </div>
+		   </div>
+	<div id="copyright">
+	    <ul>
+				
+	    </ul>
+	</div>
+		             </div>
+				</section>
+<!-- Highlights 2-->
+				<section id="highlights" class="wrapper style3">
+					
+					
+				</section>
+
+		</div>
+
+
+		
+	</body>
+</html>
